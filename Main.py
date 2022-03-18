449f94379b16137302ff1c00732fc069fe0177c8
jaimanm <jaimancoding@gmail.com>
2022-03-17 20:09:03 -0400

    got a grid

--- Main.py
+++ Main.py
@@ -3,14 +3,21 @@ from tkinter import *
 root = Tk()
 root.title('Strategic TicTacToe')
 
-# Button click method
-def buttonClick(button):
-    print('button clicked')
-
-buttons = [None] * 9
+frames = [None] * 9
 for i in range(3):
     for j in range(3):
-        index = 3 * i + j
-        buttons[index] = Button(root, text='b' + str(index), font=('Helvetica', 20), height=3, width=6, bg='SystemButtonFace', command=lambda: buttonClick(buttons[index])).grid(row=i, column=j)
+        frames[3*i+j] = Frame(
+            master=root,
+            height=50,
+            width=100,
+            bg='SystemButtonFace',
+            highlightbackground='black',
+            highlightthickness=2
+        ).grid(row=i, column=j)
+        Label(
+            master=frames[3*i+j],
+            text='label',
+            font=('Helvetica', 20), height=3, width=6, bg='red'
+        ).grid(row=i,column=j)
 
 root.mainloop()
