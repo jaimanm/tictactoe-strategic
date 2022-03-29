f3fe1ebdc98e153887b128c4f2dd5659e1dd75c4
jaimanm <jaimancoding@gmail.com>
2022-03-29 14:23:42 -0400

    something

--- main2.py
+++ main2.py
@@ -4,8 +4,10 @@ from tkinter import messagebox
 root = Tk()
 root.title('Strategic TicTacToe')
 
-def endGame():
-    pass
+def endGame(winner):
+    if winner == ' ': messagebox.showinfo('TicTacToe Strategic', "It's a tie.")
+    elif winner == 'X' or winner == 'O': messagebox.showinfo('TicTacToe Strategic', winner + ' has won!')
+    else: pass
 
 def checkWin(boardNum):
     global count
@@ -22,55 +24,93 @@ def checkWin(boardNum):
     ]
     winBoard = False
     for i in winConditions:
-        if board[i[0]] == board[i[1]] == board[i[2]] and not board[i[0]]['text'] == ' ':
+        if board[i[0]]['text'] == board[i[1]]['text'] == board[i[2]]['text'] and not board[i[0]]['text'] == ' ':
+            print(board[i[0]]['text'], 'wins on board', boardNum+1)
             boardWinners[boardNum] = board[i[0]]['text']
             winBoard = True
             count += 1
-    if boardCounts[boardNum] == 9 and winBoard == True:
+            break
+    if boardCounts[boardNum] == 9 and winBoard == False:
+        print('tie on board', boardNum)
         boardWinners[boardNum] = ' '
+        count += 1
+        winBoard = True
+
+    # case for tie across the whole board
 
     for i in winConditions:
-        if boardWinners[i[0]] == boardWinners[i[1]] == boardWinners[i[2]] and not boardWinners[i[0]] == None and not boardWinners[i[0]] == ' ':
-            endGame()
-    pass
+        if boardWinners[i[0]] == boardWinners[i[1]] == boardWinners[i[2]] and not boardWinners[i[0]] == None:
+            endGame(boardWinners[i[0]])
+            winner = boardWinners[i[0]]
+        elif count == 9:
+            endGame(' ')
+            winner = ' '
 
 def disableButtons(position):
-    for i in range(len(buttons)):
-        for j in buttons[i]:
-            if i == position[1]:
-                if j['text'] == ' ':
-                    j.config(state=NORMAL)
-                else:
-                    j.config(state=DISABLED)
-            else:
-                j.config(state=DISABLED)
 
-    for i in range(len(frames)):
-        if i == position[1]:
-            frames[i]['highlightbackground'] = 'green'
+    # NEW METHOD - CHANGE ONLY WHAT NEEDS TO BE CHANGED
+    
+    if not winner == None:
+        for board in buttons:
+            for b in board:
+                b.config(state=DISABLED)
+        for f in frames:
+            f['highlightbackground'] = 'black'
+    else:
+        # disable board with the clicked button if they just won the board
+        if not boardWinners[position[0]] == None:
+            frames[position[0]]['highlightbackground'] = 'black'
+            for i in buttons[position[0]]:
+                if boardWinners[position[0]] == 'X':
+                    i['bg'] = 'blue'
+                elif boardWinners[position[0]] == 'O':
+                    i['bg'] = 'red'
+                else:
+                    i['bg'] = 'grey'
+                i.config(state=DISABLED)
+        
+        # if the next board has been won, then make all frames available
+        if not boardWinners[position[1]] == None:
+            for i in range(len(frames)):
+                if i == position[1] or not boardWinners[i] == None: frames[i]['highlightbackground'] = 'black'
+                else: frames[i]['highlightbackground'] = 'green'
+            for i in range(len(buttons)):
+                for b in buttons[i]:
+                    if i == position[1] or not boardWinners[i] == None or not b['text'] == ' ': b.config(state=DISABLED)
+                    else: b.config(state=NORMAL)
+        # otherwise make only that frame available
         else:
-            frames[i]['highlightbackground'] = 'black'
+            for i in range(len(frames)):
+                if i == position[1]: frames[i]['highlightbackground'] = 'green'
+                else: frames[i]['highlightbackground'] = 'black'
+            for i in range(len(buttons)):
+                for b in buttons[i]:
+                    if i == position[1] and b['text'] == ' ': b.config(state=NORMAL)
+                    else: b.config(state=DISABLED)
+
 
 def bClick(b):
     global clicked
     for i in buttons:
         if b in i:
             position = [buttons.index(i), i.index(b)]
+            break
     if clicked:
         b['text'] = 'X'
-        color = 'blue'
+        b['bg'] = 'blue'
     else:
         b['text'] = 'O'
-        color = 'red'
-    b['bg'] = color
-    disableButtons(position)
+        b['bg'] = 'red'
     clicked = not clicked
     boardCounts[position[0]] += 1
     checkWin(position[0])
+    disableButtons(position)
+    endGame(winner)
 
 def reset():
-    global frames, buttons, clicked, boardCounts, boardWinners, count
+    global frames, buttons, clicked, boardCounts, boardWinners, count, winner
     boardWinners = [None] * 9
+    winner = None
     count = 0
     clicked = True # X goes first
     # create 9 frames with a tictactoe board in each
@@ -83,7 +123,7 @@ def reset():
             f = Frame(
                 master=root,
                 bg='SystemButtonFace',
-                highlightbackground='black',
+                highlightbackground='green',
                 highlightthickness=3
             )
             f.grid(row=i, column=j)
